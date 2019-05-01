44eeafb3be381155543c58ffe4f963d19dd237af
teru07310731 <for.wind.to.blow.tomorrow@gmail.com>
2019-05-01 23:08:18 +0900

    14 commit

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,8 @@
 */
 
 Route::get('/', function () {
-	return view('welcome')->withTasks([
-		'Go to the store',
-		'Go to the market',
-		'Go to work',
-		'Go to the concert'
+	return view('welcome', [
+		'foo' => 'bar',
 	]);
 });
 

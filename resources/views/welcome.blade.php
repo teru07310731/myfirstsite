f58948e0c391f31e7423db7fbb3210600a83c82d
teru07310731 <for.wind.to.blow.tomorrow@gmail.com>
2019-05-01 12:16:07 +0900

    08 commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-    <link rel="stylesheet" type="text/css" href="/css/app.css">
-</head>
-<body>
-    <h1>Here we go!!!</h1>
-	<ul>
-		<li><a href="/about">About</a>About us</li>
-		<li><a href="/contact">Contact</a>Contact</li>
-	</ul>
-</body>
-</html> 
+@extends('layout')
+
+@section('content')
+	<h1>My First Website!</h1>
+@endsection

e101a3f13981d423724ba2e30e610fb295801886
teru07310731 <for.wind.to.blow.tomorrow@gmail.com>
2019-05-01 13:59:09 +0900

    11 commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,13 @@
 @extends('layout')
 
-@section('title')
 
-@endsection
 
 @section('content')
 	<h1>My First Website!</h1>
 
 	<ul>
-		<?php foreach ($tasks as $task) : ?>
-			<li><?= $task; ?></li>
-		<?php endforeach; ?>
+		@foreach($tasks as $task)
+			<li>{{ $task }}</li>
+		@endforeach
 	</ul>
 @endsection

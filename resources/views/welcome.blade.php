925baf0cbae19617e95405cb89261da2c5236219
teru07310731 <for.wind.to.blow.tomorrow@gmail.com>
2019-05-01 11:06:52 +0900

    02 commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+<body>
+    <h1>Here we go!!!</h1>
+	<p>
+		<a href="/contact">Contact</a>Contact us to learn more.
+	</p>
+
+</body>
+</html> 

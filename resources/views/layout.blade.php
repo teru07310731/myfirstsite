7746de3a850052f099baec82ef2e8e3db8b75a15
teru07310731 <for.wind.to.blow.tomorrow@gmail.com>
2019-05-01 11:49:07 +0900

    07 commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	@yield('content')
+	<ul>
+		<li><a href="/about">About us</a></li>
+		<li><a href="/contact">Contact</a></li>
+	</ul>
+</body>
+</html>
